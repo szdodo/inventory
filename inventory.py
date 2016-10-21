e21395ee584b3b7df560bdb811a1ab8c6bdbe7ed
Szeghi Dorottya <szeghi.d@gmail.com>
2016-10-21 13:17:15 +0200

    step 3 félig, step4 kész

--- inventory.py
+++ inventory.py
@@ -8,37 +8,9 @@ def display_inventory(inventory):
         print(inventory[each] , each)
         sum += inventory[each]
     print("Total number of items: ", sum,"\n")
-        
 
-def add_to_inventoryLL(inventory,added_items):
-    exist=[]
-    for each in added_items:
-        for item in list(inventory):
-            if item == each:
-                exist=True
-                #print(each,item,"elso")
-                #inventory[item] = inventory[item]+1
-                #print(each,inventory[each])
-           #else
-                #print(each,item,"masodik")
-                #inventory[each] = 1
-                #print(each,inventory[each])
-       
-    for each in added_items:
-        print(4)
-        print(each, )
-        if exist:
-            inventory[each] += 1
-        else:
-            inventory[each] = 1
-    return inventory
 
 def add_to_inventory(inventory,added_items):
-    for item in inventory:
-        for  each in added_items:
-            if item == each:
-                inventory[item] += 1
-    
     for each in added_items:
         new_item=True
         for item in list(inventory):
@@ -46,10 +18,88 @@ def add_to_inventory(inventory,added_items):
                 new_item=False
         if new_item:
             inventory[each] = 1
+            added_items.remove(each)
+            new_item=False
 
+    for item in inventory:
+        for  each in added_items:
+            if item == each:
+                inventory[item] += 1
+            
     return inventory
 
+#szét kéne még szedni h rövidebb legyen, NINCS SORREND RENDESEN, CSAK A SZIMPLA FORMÁZÁS
+def print_table(order,inventory):
+    #Getting the max lengthed string from our inventory
+    max_length=0
+    for each in inventory:
+        if len(each)>max_length:
+            max_length=len(each)
+
+    print('Inventory:')
+    print('count'.center(max_length),'item name'.center(max_length))
+
+    #printing the line:
+    for i in range(2*max_length):
+        print('_',end="")
+    print('_')
+    sum=0
+
+    if order == "count,desc":
+        for key,value in sorted(inv.items()):
+            print(str(value).rjust(max_length), str(key).rjust(max_length)) 
+            sum += value
+        print("fff")
+    elif order == "count,asc":
+        for key,value in sorted(inv.items()):
+            print(str(value).rjust(max_length), str(key).rjust(max_length)) 
+            sum += value
+        print("aaa")
+    elif order == "":
+        for each in inventory:
+            print(str(inventory[each]).rjust(max_length) , str(each).rjust(max_length))
+            sum += inventory[each]
+    
+    #printing the line:
+    for i in range(2*max_length):
+        print('_',end="")
+    print('_')
+    print("Total number of items: ", sum,"\n")
+
+
+def import_inventory(filename,inventory):
+    givenFile=open(filename,'r')
+    data=[]
+    newInv=[]
+    firstLine=givenFile.readline()
+    #print(firstLine)
+    for line in givenFile:
+        data.append(line)
+    #print(data)
+    for each in range(len(data)):
+        darab=data[each]
+        vmi=[]
+        vmi=darab.split(',')
+        for times in range(int(vmi[1])):
+            newInv.append(vmi[0])
+    #print(newInv)
+    inventory=add_to_inventory(inventory,newInv)
+
+#KELL MAIN
 
 display_inventory(inv)
 inv = add_to_inventory(inv, dragon_loot)
 display_inventory(inv)
+print_table("count,asc",inv)
+import_inventory("test.txt",inv)
+display_inventory(inv)
+"""
+for key in sorted(inv.values()):
+    print(key)
+vmi="fsf,dsfs,ds"
+db=[]
+db=vmi.split(',')
+print(vmi.split(','))
+print(db)
+"""
+
